e40eafff61dfd6535f5bbe2e5a045af163dcebc3
Arup Naskar <40661263+arupnaskarbkp@users.noreply.github.com>
2020-08-31 21:21:04 +0530

    Update getdata.php

--- AllAppPages/getdata.php
+++ AllAppPages/getdata.php
@@ -1,47 +1,12 @@
 [
     {
         "id": "1",
-        "name": "A PostcardCandles",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/A%20PostcardCandles.jpg"
+        "name": "Arup Naskar",
+        "imageurl": "https://cdn.pixabay.com/photo/2020/05/13/17/38/foliage-5168308__340.jpg"
     },
     {
         "id": "2",
-        "name": "CarConceptVehicle",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/CarConceptVehicle.jpg"
-    },
-    {
-        "id": "3",
-        "name": "LaptopComputer",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/LaptopComputer.jpg"
-    },
-    {
-        "id": "4",
-        "name": "Lens",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/Lens.jpg"
-    },
-    {
-        "id": "5",
-        "name": "Light Bulb Pearl",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/Light%20BulbPearLamp.jpg"
-    },
-    {
-        "id": "6",
-        "name": "Modal Railway",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/Model%20Railway.jpg"
-    },
-    {
-        "id": "7",
-        "name": "PhysicsQuantum Physics",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/PhysicsQuantum%20Physics.jpg"
-    },
-    {
-        "id": "8",
-        "name": "Smart HomeHouse",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/Smart%20HomeHouse.jpg"
-    },
-    {
-        "id": "9",
-        "name": "TeamTeam Building",
-        "imageurl": "https://uniqueandrocode.000webhostapp.com/hiren/retrofit/image/TeamTeam%20Building.jpg"
+        "name": "Prafulla Naskar",
+        "imageurl": "https://cdn.pixabay.com/photo/2020/05/13/17/38/foliage-5168308__340.jpg"
     }
 ]
